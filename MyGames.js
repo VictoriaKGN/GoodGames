4e91fd35c94e2f00c07dd2f4f424465d7974d902
Victoria Kogan <93400045+VickyKPower@users.noreply.github.com>
2021-12-18 15:25:13 -0700

    Create MyGames.js

---
+++ MyGames.js
@@ -0,0 +1,74 @@
+var unadded = 
+[
+    {
+        gameName: "It Takes Two",
+        gameTags: ["Strategy", "Adventure", "Multiplayer", "3D"],
+        gameConsoles: ["PS4", "XBOX One", "PS5", "PC"],
+        gameRating: "4.9/5",
+        gamePicture: "itTakesTwo.png"
+    },
+    {
+        gameName: "Portal 2",
+        gameTags: ["Strategy", "Adventure", "Multiplayer", "3D"],
+        gameConsoles: ["PS3", "XBOX 360", "PC"],
+        gameRating: "4.8/5",
+        gamePicture: "portal2.jpg"
+    },
+    {
+        gameName: "Team Fortress 2",
+        gameTags: ["3D", "Multiplayer", "Competitive", "Action", "Shooter"],
+        gameConsoles: ["PS3", "XBOX 360", "PC"],
+        gameRating: "4.8/5",
+        gamePicture: "teamFortress.jpg"
+    },
+    {
+        gameName: "Half-Life 2",
+        gameTags: ["3D", "Strategy", "Action", "Adventure", "Shooter"],
+        gameConsoles: ["XBOX", "PC"],
+        gameRating: "4.7/5",
+        gamePicture: "halfLife.jpg"
+    },
+    {
+        gameName: "Counter-Strike: Global Offensive",
+        gameTags: ["Strategy", "Multiplayer", "Shooter", "Competitive"],
+        gameConsoles: ["PS3", "XBOX 360", "PC"],
+        gameRating: "4.4/5",
+        gamePicture: "csgo.jpg"
+    },
+    {
+        gameName: "Among Us",
+        gameTags: ["2D", "Multiplayer", "Action"],
+        gameConsoles: ["PS4", "XBOX One", "PC", "Nintendo Switch"],
+        gameRating: "3.7/5",
+        gamePicture: "amongUs.jpg"
+    },
+];
+
+var added = 
+[
+    {
+        gameName: "Mario Kart 8",
+        gamePicture: "marioKart8.jpg",
+        myRating: 4.1
+    },
+    {
+        gameName: "Call of Duty: Black Ops ",
+        gamePicture: "codBlop.jpg",
+        myRating: 4.5
+    },
+    {
+        gameName: "Minecraft",
+        gamePicture: "minecraft.png",
+        myRating: 4.0
+    },
+    {
+        gameName: "Pacman",
+        gamePicture: "pacman.jpg",
+        myRating: 3.9
+    },
+    {
+        gameName: "Wii Sports",
+        gamePicture: "wiiSports.jpg",
+        myRating: 3.6
+    },
+];
